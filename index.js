b7854a5ccb7d46ddc7ba3c54e292069e094e32b7
Aboli-SB <aboli.more@securityboat.net>
2025-03-12 14:57:58 +0530

    Implmented Event-driven architecture

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
-const details = require("./second");
-console.log("Hello World", details.name);
+const EventEmitter = require('events');
+
+const eventEmitter = new EventEmitter();
+
+eventEmitter.on('greet', (name) => {
+  console.log(`Hello, ${name}! Welcome to Node.js!`);
+});
+
+eventEmitter.emit('greet', 'Aboli');
+
+
+// const details = require("./second");
+// console.log("Hello World", details.name);
